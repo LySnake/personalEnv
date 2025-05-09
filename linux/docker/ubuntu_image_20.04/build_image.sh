cbc60bda8d00b2ee05a70ea1d775c79ddb967e13
lysnake <lysnake1024@gmail.com>
2025-05-09 20:58:20 +0800

    feat:添加ubuntu 20.04 Dockerfile

---
+++ linux/docker/ubuntu_image_20.04/build_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "###########################################################"
+echo "docker info:"
+docker info
+
+
+echo "###########################################################"
+echo "docker build:"
+dockerFile_dir=$(cd `dirname $0`; pwd)
+# echo ${dockerFile_dir}
+docker build -t ubuntu2004:1.0.0 ${dockerFile_dir}
